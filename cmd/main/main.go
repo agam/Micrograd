235f65b538305ac9c011bf4f0283efc5814aa0ef
Agam Brahma <git@agambrahma.com>
2024-10-14 17:07:10 -0700

    Added backprop (?)

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -37,10 +37,19 @@ func writeDot(v *m.Value) {
 }
 
 func main() {
-	a := m.NewValue(2.0, []*m.Value{})
-	b := m.NewValue(3.0, []*m.Value{})
-	c := a.Add(b)
+	x1 := m.NewValue(2.0, "x1")
+	x2 := m.NewValue(0.0, "x2")
 
-	writeDot(c)
+	w1 := m.NewValue(-3.0, "w1")
+	w2 := m.NewValue(1.0, "w2")
 
-}
+	b := m.NewValue(6.881373587629085, "b")
+
+	x1w1 := x1.Mul(w1, "x1*w1")
+	x2w2 := x2.Mul(w2, "x2*w2")
+
+	x1w1x2w2 := x1w1.Add(x2w2, "x1*w1 + x2*w2")
+
+	nn := x1w1x2w2.Add(b, "nn")
+	o := nn.Tanh()
+
--- pkg/micrograd/nn.go
+++ pkg/micrograd/nn.go
@@ -1,25 +1,63 @@
 package micrograd
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type Value struct {
 	Data     float64
 	Children []*Value
 	Op       string
+	Label    string
+	Grad     float64
+	Backward func()
 }
 
-func NewValue(data float64, children []*Value) *Value {
-	return &Value{Data: data, Children: children}
+func NewValue(data float64, label string) *Value {
+	return &Value{Data: data, Children: []*Value{}, Op: "", Label: label, Grad: 0.0}
 }
 
-func (v *Value) Add(other *Value) *Value {
-	return NewValue(v.Data+other.Data, []*Value{v, other})
+func NewValueWithOp(data float64, children []*Value, op string, label string) *Value {
+	return &Value{Data: data, Children: children, Op: op, Label: label}
 }
 
-func (v *Value) Mul(other *Value) *Value {
-	return NewValue(v.Data*other.Data, []*Value{v, other})
+func (v *Value) Add(other *Value, label string) *Value {
+	out := NewValueWithOp(v.Data+other.Data, []*Value{v, other}, " + ", label)
+	backward := func() {
+		v.Grad += 1.0 * out.Grad
+		other.Grad += 1.0 * out.Grad
+	}
+	out.Backward = backward
+	return out
+}
+
+func (v *Value) Mul(other *Value, label string) *Value {
+	out := NewValueWithOp(v.Data*other.Data, []*Value{v, other}, " * ", label)
+	backward := func() {
+		v.Grad += other.Data * out.Grad
+		other.Grad += v.Data * out.Grad
+	}
+	out.Backward = backward
+	return out
+}
+
+func (v *Value) Tanh() *Value {
+	x := v.Data
+	t := (math.Exp(2*x) - 1) / (math.Exp(2*x) + 1)
+	out := NewValueWithOp(t, []*Value{v}, " tanh", v.Label)
+	backward := func() {
+		v.Grad += (1 - t*t) * out.Grad
+	}
+	out.Backward = backward
+	out.Children = []*Value{v}
+
+	return out
 }
 
 func (v *Value) String() string {
-	return fmt.Sprintf("Value(%f)", v.Data)
+	if v.Op == "" {
+		return fmt.Sprintf("Value(%s => %f)", v.Label, v.Data)
+	}
+	return fmt.Sprintf("OP [ %s ] %s => %f", v.Op, v.Label, v.Data)
 }
